57111d02ef169d880117b7bb369e8e7e627c8e65
Deon T. Benton <dtbenton@andrew.cmu.edu>
2018-10-12 16:15:03 -0400

    first commits

---
+++ dyanmic_hab_ncamp.ex
@@ -0,0 +1,65 @@
+defT:-	
+name: RedSquareRectilinear	
+I:	
+(Shape)                                        
+ 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+(Color)      
+ 1 0 0
+(Path)    
+ 0 1
+T:	
+(ShapeOUT)                                        
+ 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+(ColorOUT)      
+ 1 0 0
+(PathOUT)    
+ 0 1
+;
+name: RedMuffinRectilinear	
+I:	
+(Shape)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0
+(Color)      
+ 1 0 0
+(Path)    
+ 0 1
+T:	
+(ShapeOUT)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0
+(ColorOUT)      
+ 1 0 0
+(PathOUT)    
+ 0 1
+;
+name: BlueCircleCurvilinear	
+I:	
+(Shape)                                        
+ 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0
+(Color)      
+ 0 1 0
+(PathOUT)    
+ 1 0
+T:	
+(ShapeOUT)                                        
+ 0 0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0
+(ColorOUT)      
+ 0 1 0
+(PathOUT)    
+ 1 0
+;
+name: BluePentagonCurvilinear	
+I:	
+(Shape)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0
+(Color)      
+ 0 1 0
+(PathOUT)    
+ 1 0
+T:	
+(ShapeOUT)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 0 0 0 0
+(ColorOUT)      
+ 0 1 0
+(PathOUT)    
+ 1 0
+;
---
+++ test_ncamp_new.ex
@@ -0,0 +1,40 @@
+name: Consistent	
+I:	
+(Shape)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1
+(Color)      
+ 1 0 0
+(Feature)    
+ 0 1
+(Path)    
+ 0 1
+T:	
+(ShapeOUT)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1
+(ColorOUT)      
+ 1 0 0
+(FeatureOUT)    
+ 0 1
+(PathOUT)    
+ 0 1
+;
+name: Inconsistent	
+I:	
+(Shape)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1
+(Color)      
+ 1 0 0
+(Feature)    
+ 0 1
+(Path)    
+ 1 0
+T:	
+(ShapeOUT)                                        
+ 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1
+(ColorOUT)      
+ 1 0 0
+(FeatureOUT)    
+ 0 1
+(PathOUT)    
+ 1 0
+;
